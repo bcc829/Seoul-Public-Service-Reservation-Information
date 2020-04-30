000c71be9d157e36d26530d47c6688573410a8f1
김윤희 <cdbsgmlc@naver.com>
2020-04-30 18:27:22 +0900

    PostCommentHandler 추가

---
+++ src/main/kotlin/org/jeong/reservationinformation/community/handler/comment/PostCommentHandler.kt
@@ -0,0 +1,9 @@
+package org.jeong.reservationinformation.community.handler.comment
+
+import org.jeong.reservationinformation.community.repository.PostCommentRepository
+import org.springframework.stereotype.Component
+
+@Component
+class PostCommentHandler(val postCommentRepository: PostCommentRepository) {
+
+}
